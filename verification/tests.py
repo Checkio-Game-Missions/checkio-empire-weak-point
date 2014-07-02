4afd857209949cf917be69189717980721cf1728
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-07-03 11:52:56 +1200

    base tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,19 +7,23 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [[7, 2, 7, 2, 8], [2, 9, 4, 1, 7], [3, 8, 6, 2, 4], [2, 5, 2, 9, 1], [6, 6, 5, 4, 5]],
+            "answer": [3, 3],
+            "explanation": [[26, 23, 23, 19, 26], [20, 30, 24, 18, 25]],
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": [[7, 2, 4, 2, 8], [2, 8, 1, 1, 7], [3, 8, 6, 2, 4], [2, 5, 2, 9, 1], [6, 6, 5, 4, 5]],
+            "answer": [1, 2],
+            "explanation": [[23, 19, 23, 19, 26], [20, 29, 18, 18, 25]],
+        },
+        {
+            "input": [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            "answer": [0, 0],
+            "explanation": [[3, 3, 3], [3, 3, 3]],
+        },
     ],
     "Extra": [
         {
